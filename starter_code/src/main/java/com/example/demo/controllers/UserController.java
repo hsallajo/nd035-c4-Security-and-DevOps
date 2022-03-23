77cc003a25a40255f9d2a6eb21646ac6caa9481c
shuopone <hsallajo@gmail.com>
2022-03-22 23:20:40 -0700

    adds slf4j

--- starter_code/src/main/java/com/example/demo/controllers/UserController.java
+++ starter_code/src/main/java/com/example/demo/controllers/UserController.java
@@ -2,6 +2,8 @@ package com.example.demo.controllers;
 
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,10 +24,12 @@ import com.example.demo.model.requests.CreateUserRequest;
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
-	
+
+	private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private CartRepository cartRepository;
 
@@ -36,23 +40,26 @@ public class UserController {
 	public ResponseEntity<User> findById(@PathVariable Long id) {
 		return ResponseEntity.of(userRepository.findById(id));
 	}
-	
+
 	@GetMapping("/{username}")
 	public ResponseEntity<User> findByUserName(@PathVariable String username) {
 		User user = userRepository.findByUsername(username);
 		return user == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(user);
 	}
-	
+
 	@PostMapping("/create")
 	public ResponseEntity<User> createUser(@RequestBody CreateUserRequest createUserRequest) {
+
 		User user = new User();
 		user.setUsername(createUserRequest.getUsername());
 
-		if(createUserRequest.getPassword() == null ||
+		if (createUserRequest.getPassword() == null ||
 				createUserRequest.getConfirmPassword() == null ||
 				createUserRequest.getPassword().length() < 8 ||
-				!createUserRequest.getPassword().equals(createUserRequest.getConfirmPassword()))
+				!createUserRequest.getPassword().equals(createUserRequest.getConfirmPassword())){
+			log.info("Invalid username or password.");
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+		}
 
 		// add some validation
 
@@ -62,7 +69,8 @@ public class UserController {
 		cartRepository.save(cart);
 		user.setCart(cart);
 		userRepository.save(user);
+		log.info("Created username " + createUserRequest.getUsername() + ".");
 		return ResponseEntity.ok(user);
 	}
-	
+
 }
