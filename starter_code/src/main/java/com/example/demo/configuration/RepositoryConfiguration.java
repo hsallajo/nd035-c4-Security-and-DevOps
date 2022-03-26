4a247f026b2476730355705b60bb3715644e85c1
shuopone <hsallajo@gmail.com>
2022-03-26 12:39:26 -0700

    removes redundant code from repository configuration

--- starter_code/src/main/java/com/example/demo/configuration/RepositoryConfiguration.java
+++ starter_code/src/main/java/com/example/demo/configuration/RepositoryConfiguration.java
@@ -1,13 +1,9 @@
 package com.example.demo.configuration;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
-//@EnableJpaAuditing
 @EnableJpaRepositories(basePackages = "com.example.demo.model.persistence.repositories")
-//@EntityScan("com.example.demo.model.persistence")
 public class RepositoryConfiguration {
 }
