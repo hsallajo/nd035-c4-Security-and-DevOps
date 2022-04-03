9211b2308e6422d9e3aeece7f1c227b38448a045
shuopone <hsallajo@gmail.com>
2022-04-02 19:06:58 -0700

    updates app to extend SpringBootServletInitializer

--- starter_code/src/main/java/com/example/demo/DemoApplication.java
+++ starter_code/src/main/java/com/example/demo/DemoApplication.java
@@ -6,12 +6,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EntityScan("com.example.demo.model.persistence")
 @SpringBootApplication(exclude={SecurityAutoConfiguration.class})
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 
 	private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
 	private static final String DEMO_APP_MAIN = "API_MAIN: ";
